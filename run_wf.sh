362fd7a0bdafb2850cc9d8078096ca38fcace0fc
George Papadimitriou <georgpap@isi.edu>
2019-11-07 19:16:59 -0800

    echo daxfile before planning

--- run_wf.sh
+++ run_wf.sh
@@ -8,6 +8,7 @@ daxgen_out=$(python daxgen.py -o dax_outputs -p pegasus.default.properties -r rc
 dax_file=$(echo $daxgen_out | cut -d ' ' -f 1)
 properties_file=$(echo $daxgen_out | cut -d ' ' -f 2)
 
+echo $dax_file
 ./plan.sh ${dax_file} ${properties_file}
 
 cd $old_dir
