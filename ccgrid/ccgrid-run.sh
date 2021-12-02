ce8eadd243dc854e6690f0a06c4a48927dea79ac
Ryan Tanaka <tanaka@isi.edu>
2021-12-02 23:29:31 +0000

    master run script, redoing runs because need to disable cleanup to avoid container bug

--- ccgrid/ccgrid-run.sh
+++ ccgrid/ccgrid-run.sh
@@ -1,7 +1,60 @@
 #!/bin/bash
 
-for i in {7..10}; do
+### EDGE ONLY RUNS ####################################### 
+python3 generate-sc.py --edge-only
+
+for i in {1..10}; do
+    TOP_DIR=EDGE-RUN0$i
+    for FILE in ../invoke_90sec/*.yml;
+    do
+        echo "CURRENT FILE: $FILE"
+        pegasus-plan \
+        -Dpegasus.catalog.replica.file=edge_rc \
+        -Dpegasus.catalog.site.file=sites.yml \
+        -Dpegasus.mode=development \
+        -Dpegasus.integrity.checking=none \
+        -Dpegasus.data.configuration=nonsharedfs \
+        -Dpegasus.transfer.bypass.input.staging=True \
+        -Dpegasus.monitord.encoding=json \
+        -Dpegasus.catalog.workflow.amqp.url=amqp://friend:donatedata@msgs.pegasus.isi.edu:5672/prod/workflow \
+        -Dpegasus.transfer.links=True \
+        --dir $TOP_DIR \
+        --output-sites=local \
+        --sites=condorpool \
+        --cleanup none \
+        --staging-site condorpool=condorpool \
+        --force \
+        --cluster label \
+        $FILE
+        
+    done
+
+    
+    start=$(date +%s)
+    for submit_dir in ./$TOP_DIR/panorama/pegasus/casa-wind/*
+    do
+        pegasus-run $submit_dir
+    done
+    
+    while true; do
+        if pegasus-status | grep -q "no matching jobs"; then
+            break
+        fi
+        clear
+        pegasus-status -l
+        sleep 1
+    done
+    end=$(date +%s)
+
+    echo "$((end-start))" > $TOP_DIR/duration.txt
+done
+
+### EDGE CLOUD RUNS ####################################### 
+python3 generate-sc.py
+
+for i in {1..10}; do
     TOP_DIR=EDGE-CLOUD-RUN0$i
+
     for FILE in ../invoke_90sec/*.yml;
     do
         echo "CURRENT FILE: $FILE"
@@ -18,11 +71,14 @@ for i in {7..10}; do
         --dir $TOP_DIR \
         --output-sites=local \
         --sites=condorpool \
+        --cleanup none \
         --staging-site condorpool=staging \
         --force \
         --cluster label \
         $FILE
+
     done
+
     
     start=$(date +%s)
     for submit_dir in ./$TOP_DIR/panorama/pegasus/casa-wind/*
@@ -35,10 +91,62 @@ for i in {7..10}; do
             break
         fi
         clear
-        pegasus-status
+        pegasus-status -l
         sleep 1
     done
     end=$(date +%s)
 
     echo "$((end-start))" > $TOP_DIR/duration.txt
 done
+
+### CLOUD RUNS ####################################### 
+python3 generate-sc.py --cloud-only
+
+for i in {1..10}; do
+    TOP_DIR=CLOUD-RUN0$i
+
+    for FILE in ../invoke_90sec/*.yml;
+    do
+        echo "CURRENT FILE: $FILE"
+        pegasus-plan \
+        -Dpegasus.catalog.replica.file=cloud_rc \
+        -Dpegasus.catalog.site.file=sites.yml \
+        -Dpegasus.mode=development \
+        -Dpegasus.integrity.checking=none \
+        -Dpegasus.data.configuration=nonsharedfs \
+        -Dpegasus.transfer.bypass.input.staging=True \
+        -Dpegasus.monitord.encoding=json \
+        -Dpegasus.catalog.workflow.amqp.url=amqp://friend:donatedata@msgs.pegasus.isi.edu:5672/prod/workflow \
+        -Dpegasus.transfer.links=True \
+        --dir $TOP_DIR \
+        --output-sites=local \
+        --sites=condorpool \
+        --cleanup none \
+        --staging-site condorpool=staging \
+        --force \
+        --cluster label \
+        $FILE
+        
+    done
+
+    
+    start=$(date +%s)
+    for submit_dir in ./$TOP_DIR/panorama/pegasus/casa-wind/*
+    do
+        pegasus-run $submit_dir
+    done
+    
+    while true; do
+        if pegasus-status | grep -q "no matching jobs"; then
+            break
+        fi
+        clear
+        pegasus-status -l
+        sleep 1
+    done
+    end=$(date +%s)
+
+    echo "$((end-start))" > $TOP_DIR/duration.txt
+done
+
+
